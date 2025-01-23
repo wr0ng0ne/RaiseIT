5817a4efba982e4f2398bc5d2314365e288f6281
Intisar <official.tasnimhaider@gmail.com>
2025-01-23 07:40:32 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
+const styles = {
+  global:(props)=>({
+    body:{
+      color:mode('gray.800','whiteAlpha.900')(props),
+      bg:mode('gray.100','#101010')(props),
+    }
+  })
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true
+};
+
+const colors = {
+  grey:{
+    light: "#616161",
+    dark: "#1e1e1e"
+  }
+};
+
+const theme = extendTheme({config, styles, colors});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+    <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
